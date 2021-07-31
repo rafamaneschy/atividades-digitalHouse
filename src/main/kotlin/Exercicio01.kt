efbd661a1345d2302cf909f7f8c271b2199b2401
Rafael Maneschy <rafamaneschy@gmail.com>
2021-07-31 12:46:14 -0300

    Exercicio01

---
+++ src/main/kotlin/Exercicio01.kt
@@ -0,0 +1,8 @@
+fun maiorValor(numA: Int, numB: Int, numC: Int): Int {
+    return if (numA > numB && numA > numC) numA
+    else if (numB > numC) numB
+    else numC
+}
+fun main() {
+    println(maiorValor(10,15,20))
+}
